d3dc281736215fab96166dea4deb37dbb7cfd45b
ronek22 <jronek3010@gmail.com>
2018-03-20 15:55:10 +0100

    Bootstrap and some styling

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -28,9 +28,11 @@ class StudentsController < ApplicationController
 
     respond_to do |format|
       if @student.save
-        format.html { redirect_to @student, notice: 'Student was successfully created.' }
+        flash[:success] = 'Student was successsfully created.'
+        format.html { redirect_to @student }
         format.json { render :show, status: :created, location: @student }
       else
+        flash[:danger] = "There was a problem creating the Student."
         format.html { render :new }
         format.json { render json: @student.errors, status: :unprocessable_entity }
       end
@@ -42,9 +44,11 @@ class StudentsController < ApplicationController
   def update
     respond_to do |format|
       if @student.update(student_params)
-        format.html { redirect_to @student, notice: 'Student was successfully updated.' }
+        flash[:success] = "Student was successfully updated."
+        format.html { redirect_to @student }
         format.json { render :show, status: :ok, location: @student }
       else
+        flash[:danger] = "There was a problem updating the Student."
         format.html { render :edit }
         format.json { render json: @student.errors, status: :unprocessable_entity }
       end
@@ -56,7 +60,8 @@ class StudentsController < ApplicationController
   def destroy
     @student.destroy
     respond_to do |format|
-      format.html { redirect_to students_url, notice: 'Student was successfully destroyed.' }
+      flash[:success] = "Student was successfully destroyed."
+      format.html { redirect_to students_url }
       format.json { head :no_content }
     end
   end
